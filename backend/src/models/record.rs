08dd47ef161811c5d7f5dcf194454a1983bde489
Lorenzo Carbonell a.k.a. atareao <lorenzo.carbonell.cerezo@gmail.com>
2025-10-07 07:41:18 +0200

    🚀 Añade funciones CRUD y análisis por país y ciudad en el modelo Record

--- backend/src/models/record.rs
+++ backend/src/models/record.rs
@@ -43,5 +43,76 @@ impl Record{
             created_at: row.get("created_at"),
         }
     }
+
+    pub async fn create( pool: &PgPool, record: Record) -> Result<Record, Error> {
+
+        let sql = "INSERT INTO records (ip_address, protocol, fqdn, path, city_name, country_name, country_code, rule_id, created_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *";
+        let now = Utc::now();
+        query(sql)
+            .bind(record.ip_address)
+            .bind(record.protocol)
+            .bind(record.fqdn)
+            .bind(record.path)
+            .bind(record.city_name)
+            .bind(record.country_name)
+            .bind(record.country_code)
+            .bind(record.rule_id)
+            .bind(now)
+            .map(Self::from_row)
+            .fetch_one(pool)
+            .await
+    }
+
+    pub async fn read(pool: &PgPool, id: i32) -> Result<Record, Error> {
+        let sql = "SELECT * FROM records WHERE id = $1";
+        query(sql)
+            .bind(id)
+            .map(Self::from_row)
+            .fetch_one(pool)
+            .await
+    }
+    pub async fn read_all(pool: &PgPool) -> Result<Vec<Record>, Error> {
+        let sql = "SELECT * FROM records";
+        query(sql)
+            .map(Self::from_row)
+            .fetch_all(pool)
+            .await
+    }
+
+    pub async fn select_by_country(pool: &PgPool) -> Result<Vec<(String, i64)>, Error> {
+        let sql = "SELECT country_name, COUNT(*) as count FROM records GROUP BY country_name";
+        query(sql)
+            .map(|row: PgRow| {
+                let country_name: String = row.get("country_name");
+                let count: i64 = row.get("count");
+                (country_name, count)
+            })
+            .fetch_all(pool)
+            .await
+    }
+
+    pub async fn select_by_city(pool: &PgPool) -> Result<Vec<(String, i64)>, Error> {
+        let sql = "SELECT city_name, COUNT(*) as count FROM records GROUP BY city_name";
+        query(sql)
+            .map(|row: PgRow| {
+                let country_name: String = row.get("city_name");
+                let count: i64 = row.get("count");
+                (country_name, count)
+            })
+            .fetch_all(pool)
+            .await
+    }
+
+    pub async fn delete_before(pool: &PgPool, days: i32) -> Result<Vec<Record>, Error> {
+        let sql = "DELETE FROM records WHERE created_at > $1 RETURNING *";
+        let now = Utc::now()
+            .checked_sub_signed(chrono::Duration::days(days.into())).unwrap();
+        query(sql)
+            .bind(now)
+            .map(Self::from_row)
+            .fetch_all(pool)
+            .await
+    }
+
 }
 
