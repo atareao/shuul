46fc2f04ff4b99b7f4797887faee833a5dbf17d7
Lorenzo Carbonell a.k.a. atareao <lorenzo.carbonell.cerezo@gmail.com>
2025-10-22 08:37:48 +0200

    ✨ Actualiza código Rust para manejar errores en consulta de IP y elimina dependencias innecesarias en frontend y configuración de Vite

--- backend/src/models/ipdata.rs
+++ backend/src/models/ipdata.rs
@@ -1,9 +1,6 @@
-use serde::{Serialize, Deserialize};
-use maxminddb::{
-    Reader,
-    geoip2,
-};
-use tracing::debug;
+use maxminddb::{Reader, geoip2};
+use serde::{Deserialize, Serialize};
+use tracing::{debug, error};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct IPData {
@@ -14,32 +11,42 @@ pub struct IPData {
 }
 
 impl IPData {
-    pub fn complete(maxmind_db: &Reader<Vec<u8>>, ip_address: &str) -> Self{
+    pub fn complete(maxmind_db: &Reader<Vec<u8>>, ip_address: &str) -> Self {
         debug!("Look data for ip address: {}", ip_address);
-        let ip = ip_address.parse().unwrap();
-        debug!("Look data for ip: {:?}", ip);
-        match maxmind_db.lookup::<geoip2::City>(ip).unwrap_or_default() {
-            Some(result) => {
-                debug!("result: {:?}", result);
-                let country = result.country.as_ref();
-                Self {
-                    ip_address: ip_address.to_string(),
-                    city_name: result
-                        .city
-                        .and_then(|c| c.names)
-                        .and_then(|n| n.get("en").cloned())
-                        .map(|s| s.to_string()),
-                    country_name: country
-                        .and_then(|c| c.names.clone())
-                        .and_then(|n| n.get("en").cloned())
-                        .map(|s| s.to_string()),
-                    country_code: result
-                        .country
-                        .and_then(|c| c.iso_code)
-                        .map(|s| s.to_string()),
+        match ip_address.parse() {
+            Ok(ip) => {
+                debug!("Look data for ip: {:?}", ip);
+                match maxmind_db.lookup::<geoip2::City>(ip).unwrap_or_default() {
+                    Some(result) => {
+                        debug!("result: {:?}", result);
+                        let country = result.country.as_ref();
+                        Self {
+                            ip_address: ip_address.to_string(),
+                            city_name: result
+                                .city
+                                .and_then(|c| c.names)
+                                .and_then(|n| n.get("en").cloned())
+                                .map(|s| s.to_string()),
+                            country_name: country
+                                .and_then(|c| c.names.clone())
+                                .and_then(|n| n.get("en").cloned())
+                                .map(|s| s.to_string()),
+                            country_code: result
+                                .country
+                                .and_then(|c| c.iso_code)
+                                .map(|s| s.to_string()),
+                        }
+                    }
+                    None => Self {
+                        ip_address: ip_address.to_string(),
+                        city_name: None,
+                        country_name: None,
+                        country_code: None,
+                    },
                 }
-            },
-            None => {
+            }
+            Err(e) => {
+                error!("Look data for ip: {:?}: {}", ip_address, e);
                 Self {
                     ip_address: ip_address.to_string(),
                     city_name: None,
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,21 @@
 import path from 'path'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: 'src/assets/logo.svg',
+                    dest: 'images'
+                }
+            ]
+        })
+    ],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, 'src/'),
