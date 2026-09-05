c809e1cbaae23dd9b090970fb3f90951d8f9031a
Lorenzo Carbonell a.k.a. atareao <lorenzo.carbonell.cerezo@gmail.com>
2026-09-05 09:49:07 +0200

    ♻️ refactor(core): rename report_warn to report_skip

--- frontend/src/pages/admin/logs_page.tsx
+++ frontend/src/pages/admin/logs_page.tsx
@@ -13,10 +13,7 @@ import {
   Card,
   message,
 } from "antd";
-import {
-  EyeOutlined,
-  ReloadOutlined,
-} from "@ant-design/icons";
+import { EyeOutlined, ReloadOutlined } from "@ant-design/icons";
 import type { ColumnsType } from "antd/es/table";
 import { loadData } from "@/common/utils";
 import { BASE_URL } from "@/constants";
